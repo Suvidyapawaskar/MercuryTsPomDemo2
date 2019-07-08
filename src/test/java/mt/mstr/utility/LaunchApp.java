12829cf4df499e16981b494202e66efeddcfc020
Suvidyapawaskar <suvidyapawaskar@gmail.com>
2019-07-08 11:25:15 +0530

    updated launchapp

--- src/test/java/mt/mstr/utility/LaunchApp.java
+++ src/test/java/mt/mstr/utility/LaunchApp.java
@@ -13,23 +13,26 @@ public class LaunchApp
 	{
 		System.setProperty("webdriver.chrome.driver", "F:\\Selenium Setup\\setup1\\4052019\\chromedriver_win32 (22)\\chromedriver.exe");
 		Config.driver=new ChromeDriver();
+		System.out.println("Chrome browser is opened successfully");
 		
 	}
 	
     public void enterApplicationURL(String url)
     {
     	Config.driver.get(url);
+    	System.out.println("Application url is entered successfully");
     }
     
     public void maximizeBrowser()
     {
     	Config.driver.manage().window().maximize();
+    	System.out.println("Maximized browser");
     }
     
     public void waittillLoginpageLoaded(int timeoutseconds)
     {
     	Config.driver.manage().timeouts().pageLoadTimeout(timeoutseconds, TimeUnit.SECONDS);
-    	
+    	System.out.println("Wait till loginpage loaded");
     }
     
     public static void main(String[] args) 
